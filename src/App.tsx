de8908d844435b8df6bcf1cc42405a52838617eb
Sadikshya <sadikshyeah@gmail.com>
2026-08-12 00:09:37 +0300

    Implement scroll reveal animations for hero section and project cards

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,11 @@ import { Header } from './components/Header'
 import { Hero } from './components/Hero'
 import { Projects } from './components/Projects'
 import { Skills } from './components/Skills'
+import { useRevealOnScroll } from './hooks/useRevealOnScroll'
 
 function App() {
+  useRevealOnScroll()
+
   return (
     <>
       <Header />
